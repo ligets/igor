5fb72243b3b96de46b184f17bb32e748b50f5f8c
ligets <legen2a208@gmail.com>
2024-12-10 23:57:33 +0400

    asd

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
+use App\Models\Cart;
 use App\Models\Order;
 use App\Models\Status;
 use Illuminate\Http\Request;
@@ -30,11 +32,46 @@ class OrderController extends Controller
 
     public function store() {
         $req = request()->validate([
-            'type' => 'required|string'
+            'type' => 'required|string',
+            'book_id' => 'integer|min:0',
+            'quantity' => 'integer|min:0'
         ]);
         if ($req->type == 'cart') {
-            $items =
+            $cartSummary = Cart::where('user_id', auth()->id())
+                ->with('book')
+                ->get()
+                ->map(function ($cart) {
+                    return [
+                        'book_id' => $cart->book_id,
+                        'quantity' => $cart->quantity,
+                        'total_price' => $cart->quantity * $cart->book->price,
+                    ];
+                });
+            $totalPrice = $cartSummary->sum('total_price');
+            $order = Order::create([
+                'total_price' => $totalPrice,
+                'user_id' => auth()->id(),
+                'status_id' => Status::where('name', 'В обработке')->first()->id
+            ]);
+            foreach ($cartSummary as $item) {
+                $order->books()->attach([
+                    $item->book_id => ['quantity' => $item->quantity]
+                ]);
+            }
         }
+        else if ($req->type == 'book') {
+            $book = Book::findOrFail($req->book_id);
+            $order = Order::create([
+                'total_price' => $book->price * $req->quantity,
+                'user_id' => auth()->id(),
+                'status_id' => Status::where('name', 'В обработке')->first()->id
+            ]);
+            $order->books()->attach($book->id, ['quantity' => $req->quantity]);
+        }
+        else {
+            abort(400);
+        }
+        return view('', compact('order'));
     }
 
 }
