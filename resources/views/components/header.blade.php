5de053b9a0ad09cb26671f87950a958ee002ad63
ligets <legen2a208@gmail.com>
2024-12-10 20:57:35 +0400

    asd

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -18,79 +18,78 @@
             </style>
     </head>
     <body class="font-sans flex-col   antialiased bg-gray-100 w-[100%] ">
-                    <header class="flex text-[#72716E] items-center gap-2 py-10 lg:grid-cols-3 justify-around">
-                    <div>
-                    <p>POCO</p>
-                    </div>
-                        <div class="flex lg:justify-center gap-[10px]">
-                            <a>
-                            <p>Главная</p>
-                            </a>
-                            <a>
-                            <p>О нас</p>
-                            </a>
-                            <a>
-                            <p>Где мы</p>
-                            </a>
-                        </div>
-                
-                            <nav class="-mx-3 flex justify-end">
-                                @auth
-                                    <div class="hidden sm:flex sm:items-center sm:ms-6">
-                                        <x-dropdown align="right" width="48">
-                                            <x-slot name="trigger">
-                                                <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
-                                                    <div>{{ Auth::user()->name }}</div>
-
-                            <div class="ms-1">
-                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
-                                     viewBox="0 0 20 20">
-                                    <path fill-rule="evenodd"
-                                          d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                          clip-rule="evenodd"/>
-                                </svg>
-                            </div>
-                        </button>
-                    </x-slot>
-
-                    <x-slot name="content">
-                        <x-dropdown-link :href="route('profile.edit')">
-                            {{ __('Profile') }}
-                        </x-dropdown-link>
-
-                        <!-- Authentication -->
-                        <form method="POST" action="{{ route('logout') }}">
-                            @csrf
-
-                            <x-dropdown-link :href="route('logout')"
-                                             onclick="event.preventDefault();
-                                                                        this.closest('form').submit();">
-                                {{ __('Log Out') }}
-                            </x-dropdown-link>
-                        </form>
-                    </x-slot>
-                </x-dropdown>
+        <header class="flex text-[#72716E] items-center gap-2 py-10 lg:grid-cols-3 justify-around">
+            <div>
+                <p>POCO</p>
             </div>
-        @endauth
-        @guest
-            <a
-                href="{{ route('login') }}"
-                class="rounded-md px-3 py-2 bg-blue-500 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-            >
-                Log in
-            </a>
+            <div class="flex lg:justify-center gap-[10px]">
+                <a>
+                    <p>Главная</p>
+                </a>
+                <a>
+                    <p>О нас</p>
+                </a>
+                <a>
+                    <p>Где мы</p>
+                </a>
+            </div>
+            <nav class="-mx-3 flex justify-end">
+                @auth
+                    <div class="hidden sm:flex sm:items-center sm:ms-6">
+                        <x-dropdown align="right" width="48">
+                            <x-slot name="trigger">
+                                <button
+                                    class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
+                                    <div>{{ Auth::user()->name }}</div>
 
-                                 
-                                        <a
-                                            href="{{ route('register') }}"
-                                            class="rounded-md bg-blue-500s px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-                                        >
-                                            Register
-                                        </a>
-                                    
-                                @endguest
-                            </nav>
-                        
-                    </header>
+                                    <div class="ms-1">
+                                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
+                                             viewBox="0 0 20 20">
+                                            <path fill-rule="evenodd"
+                                                  d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                  clip-rule="evenodd"/>
+                                        </svg>
+                                    </div>
+                                </button>
+                            </x-slot>
+
+                            <x-slot name="content">
+                                <x-dropdown-link :href="route('profile.edit')">
+                                    {{ __('Profile') }}
+                                </x-dropdown-link>
+
+                                <!-- Authentication -->
+                                <form method="POST" action="{{ route('logout') }}">
+                                    @csrf
+
+                                    <x-dropdown-link :href="route('logout')"
+                                                     onclick="event.preventDefault();
+                                                                                this.closest('form').submit();">
+                                        {{ __('Log Out') }}
+                                    </x-dropdown-link>
+                                </form>
+                            </x-slot>
+                        </x-dropdown>
+                    </div>
+                @endauth
+                @guest
+                    <a
+                        href="{{ route('login') }}"
+                        class="rounded-md px-3 py-2 bg-blue-500 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                    >
+                        Log in
+                    </a>
+
+
+                    <a
+                        href="{{ route('register') }}"
+                        class="rounded-md bg-blue-500s px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                    >
+                        Register
+                    </a>
+
+                @endguest
+            </nav>
+        </header>
     </body>
 </html>
